9201d3933916bef5009d586fcf4a9f8a02383fec
omens-sdn <55347688+omens-sdn@users.noreply.github.com>
2020-04-15 12:00:40 +0800

    Add files via upload

---
+++ 1th/one.java
@@ -0,0 +1,7 @@
+public class one {
+		public static void main(String[] args) {
+			System.out.println("Welcome to java!");
+			System.out.println("Welcome to Coumputer Science!");
+			System.out.println("Programming is fun!");
+			}
+		}
---
+++ 1th/two.java
@@ -0,0 +1,9 @@
+public class two {
+public static void main(String[] args) {
+	System.out.println("Welcome to java!");
+	System.out.println("Welcome to java!");
+	System.out.println("Welcome to java!");
+	System.out.println("Welcome to java!");
+	System.out.println("Welcome to java!");
+}
+}
